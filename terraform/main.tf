f4c635d766441afa3ce66d2cd436ad28cc79e7f6
Vedant Patel <imvedant369@gmail.com>
2026-03-24 16:53:08 +0530

    Added CI/CD pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "demo" {
     for_each = toset (var.bucket_name)
     
-    bucket = "example-${each.key}-${var.environment}"
+    bucket = "example-${each.value}-${var.environment}"
     tags = {
         Name = each.value
         Enviornment = var.environment
